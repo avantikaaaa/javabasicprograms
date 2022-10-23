70138ba9cbae9c72317898ba7cad9aec2cc70ae1
Avantika Chauhan <101965370+avantikaaaa@users.noreply.github.com>
2022-10-23 23:15:02 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,30 +1,41 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class Array_binary {
+public class demo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr = { 10, 20, 30, 40, 50, 60, 70, 72, 76, 80 };
-		System.out.println(Arrays.toString(arr));
-		System.out.println(Search(arr, 80));
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // int[] arr = { 10, 20, 30, 40, 50, 60, 70, 72, 76, 80 };
+        System.out.print("Enter the length of the array: ");
+        Scanner input = new Scanner(System.in);
+        int l = input.nextInt();
+        int[] arr = new int[l];
+        System.out.print("Enter the elements of the array: ");
+        for (int i = 0; i < l; i++) {
+            arr[i] = input.nextInt();
+        }
+        System.out.print("Enter the element you want to search ");
+        int target = input.nextInt();
+        System.out.print("Element found at index");
+        System.out.println(Search(arr, target));
 
-	}
+    }
 
-	public static int Search(int[] arr, int ele) {
-		int li = 0;
-		int hi = arr.length - 1;
-		while (li <= hi) {
-			int mid = (li + hi) / 2;
-			if (arr[mid] == ele) {
-				return mid;
-			} else if (arr[mid] > ele) {
-				hi = mid - 1;
-			} else {
-				li = mid + 1;
-			}
-		}
-		return -1;
-	}
+    public static int Search(int[] arr, int ele) {
+        int li = 0;
+        int hi = arr.length - 1;
+        while (li <= hi) {
+            int mid = (li + hi) / 2;
+            if (arr[mid] == ele) {
+                return mid;
+            } else if (arr[mid] > ele) {
+                hi = mid - 1;
+            } else {
+                li = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
 
 // Output
